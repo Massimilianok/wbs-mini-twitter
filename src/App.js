7af190ec6332dbaa17acfa62ba602843578c9d18
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-02-26 10:47:28 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <Container fluid>
+      <div>
+        header
+        <div>Searchbar</div>
+      </div>
+      <div>user</div>
+      <main>
+        <div>message</div>
+      </main>
+      <footer>footer</footer>
+    </Container>
+  );
+}
+
+export default App;
